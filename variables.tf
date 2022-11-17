6b809d6173c687ab63ded3ae3627f8f02ec2af25
angelik.mag <angelik.mag@gmail.com>
2022-11-17 05:05:44 -0500

    eliminan credenciales

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "access_key" {
   type =  string
-  default = "AKIASNEVN2PTWKMTS4FA"
+  default = " "
 }
 
 variable "secret_key" {
     type =  string
-   default = "/M2c6prEJhC0F68wyodC2fmHhi5LVKadEdMu+bSm"
+   default = " "
 }
